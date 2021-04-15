6882a241856cda20562463a22e76c35412389768
akash chhetri <akash.chhetri1@gmail.com>
2021-04-16 09:13:02 +1000

    events search filtered trigger change of location and filtered events method triggered

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,13 +1,21 @@
 import { Fragment } from "react";
+import { useRouter } from "next/router";
 import EventList from "../../components/events/EventList";
 import EventsSearch from "../../components/events/EventsSearch";
 import { getAllEvents } from "../../dummy-data";
 
 function AllEventsPage(props) {
 	const events = getAllEvents();
+	const router = useRouter();
+
+	function findEventsHandler(year, month) {
+		const fullPath = `/events/${year}/${month}`;
+		router.push(fullPath);
+	}
+
 	return (
 		<Fragment>
-			<EventsSearch />
+			<EventsSearch onSearch={findEventsHandler} />
 			<EventList items={events} />
 		</Fragment>
 	);
